391d51583b8176843c0056cb2f55634d49e86dae
Daniel Rodriguez <daniel.b.rodriguez@gmail.com>
2021-05-04 23:55:45 -0400

    added node and express server for the backend

--- server.js
+++ server.js
@@ -27,75 +27,159 @@ app.get('/', (req, res) => {
     res.sendFile(path.join(__dirname + '/index.html'));
 });
 
-app.post('/auth', function(request, response) {
-	console.log(request.body);
-	var userEmail = request.body['user-email'];
-	var userPassword = request.body['user-password'];
-	var accountType = request.body['account-type'];
-	if (userEmail && userPassword) {
-		connection.query('SELECT * FROM accounts WHERE username = ? AND password = ?', [userEmail, userPassword], function(error, results, fields) {
+app.post('/auth', (req,res) => {
+	console.log(req.body);
+	//grab the field inputs
+	var userEmail = req.body['user-email'];
+	var userPassword = req.body['user-password'];
+	var accountType = req.body['account-type'];
+	if (userEmail && userPassword && accountType) {
+		connection.query('SELECT * FROM accounts WHERE user_email = ? AND user_password = ? AND user_type = ?', [userEmail, userPassword, accountType], function(error, results, fields) {
 			if (results.length > 0) {
-				request.session.loggedin = true;
-				request.session.userEmail = userEmail;
-				console.log('login is ' + request.session.loggedin);
-				response.redirect('/home');
+				req.session.loggedin = true;
+				req.session.userEmail = userEmail;
+				req.session.accountType = accountType;
+				console.log('login is ' + req.session.loggedin);
+				res.redirect('/home');
 			} else {
-				response.send('Incorrect Username and/or Password!');
+				res.redirect('/incorrect-user');
 			}			
-			response.end();
+			res.end();
 		});
 	} else {
-		response.send('Please enter Username and Password!');
-		response.end();
+		res.send('Please enter Username and Password!');
+		res.end();
 	}
 });
 
+app.post('/register', (req,res) => {
+	console.log(req.body);
+	//grab the field inputs
+	var firstName = req.body['first-name'];
+	var lastName = req.body['last-name'];
+	var userEmail = req.body['user-email'];
+	var confirmUserEmail = req.body['confirm-user-email'];
+	var userPassword = req.body['user-password'];
+	var confirmUserPassword = req.body['confirm-user-password'];
+	var accountType = req.body['account-type'];
+	//check if emails and passwords match
+	console.log("user email and passwords match:" + (userEmail === confirmUserEmail && userPassword === confirmUserPassword));
+
+	//check if user email already exists in the database
+	if (userEmail) {
+		connection.query('SELECT * FROM accounts WHERE user_email = ?', userEmail, function(error, results, fields) {
+			if (results.length > 0) {
+				console.log("Email already exists!");
+				res.redirect('/signup');
+			} else {
+				if (userEmail === confirmUserEmail && userPassword === confirmUserPassword) {
+
+					//insert into accounts table
+					connection.query('INSERT INTO accounts (user_email, user_password, user_type) VALUES ( ?, ?, ?)', [userEmail, userPassword, accountType], function(error, results, fields) {
+						if (error) throw error;
+						console.log("1 record inserted into accounts");		
+						res.end();
+					});
+					//insert into students table
+					connection.query('INSERT INTO students (user_ID, first_name, last_name) VALUES (LAST_INSERT_ID(), ?, ?)', [firstName, lastName], function(error, results, fields) {
+						if (error) throw error;
+						console.log("1 record inserted into students");		
+						res.end();
+					});
+					res.redirect('/login');
+				} else {
+					console.log("Passwords don't match!");
+					res.redirect('/signup');
+				}
+			}		
+			res.end();
+		});
+	}
+	else {
+		res.send('Please enter Username and Password!');
+		res.end();
+	}
+})
+
 app.get('/login', (req,res) => {
-	console.log(req.session.loggedin);
+	console.log("login session is " + req.session.loggedin);
 	req.session.loggedin = false;
-	console.log(req.session.loggedin);
+	console.log("login session is " + req.session.loggedin);
     res.sendFile(path.join(__dirname + '/index.html'));
 });
 
 app.get('/signup', (req,res) => {
-	console.log(req.session.loggedin);
+	console.log("login session is " + req.session.loggedin);
 	req.session.loggedin = false;
-	console.log(req.session.loggedin);
+	console.log("login session is " + req.session.loggedin);
     res.sendFile(path.join(__dirname + '/signup.html'));
 });
 
+app.get('/incorrect-user', (req,res) => {
+	res.sendFile(path.join(__dirname + '/incorrect-user.html'));
+});
+
 app.get('/home', (req,res) => {
+	console.log("you are on home page");
+	console.log("login session is " + req.session.loggedin);
+
 	if (req.session.loggedin) {
 		res.redirect('/homepage.html');
 	} else {
-		res.send('Please login to view this page!');
+		res.redirect('/login-error.html');
 	}
 	res.end();
 });
 
 app.get('/course-search', (req,res) => {
+	console.log("you are on course search");
+	console.log("login session is " + req.session.loggedin);
+
 	if (req.session.loggedin) {
 		res.redirect('/course-search.html');
 	} else {
-		res.send('Please login to view this page!');
+		res.redirect('/login-error.html');
 	}
 	res.end();
 });
 
 app.get('/course-planner', (req,res) => {
+	console.log("you are on course planner");
+	console.log("login session is " + req.session.loggedin);
+
 	if (req.session.loggedin) {
 		res.redirect('/course-planner.html');
 	} else {
-		res.send('Please login to view this page!');
+		res.redirect('/login-error.html');
 	}
 	res.end();
 });
 
 app.get('/course-enrollment', (req,res) => {
+	console.log("you are on course enrollment");
+	console.log("login session is " + req.session.loggedin);
+
 	if (req.session.loggedin) {
 		res.redirect('/course-enrollment.html');
 	} else {
-		res.send('Please login to view this page!');
+		res.redirect('/login-error.html');
+	}
+	res.end();
+});
+
+app.get('/manage-courses', (req,res) => {
+	console.log("you are on course search");
+	console.log("login session is " + req.session.loggedin);
+	console.log(req.session.accountType);
+
+	if (req.session.loggedin && req.session.accountType === 'faculty') {
+		res.redirect('/manage-courses.html');
+	} 
+	else if (req.session.accountType != 'faculty') {
+		res.redirect('/manage-courses-error.html');
+	}
+	else {
+		res.redirect('/login-error.html');
 	}
 	res.end();
 });
