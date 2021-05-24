1eee68c11ccb1b75aa25c5b9bfa3a28348a0e36b
RodriguezNadine <nadine.rodriguez@gmail.com>
2021-05-24 14:55:28 -0400

    fixed homepage dashboard

--- public/JavaScript/getDashboardData.js
+++ public/JavaScript/getDashboardData.js
@@ -26,12 +26,20 @@ async function getUserInformation() {
         console.log(data.length);
     }
     localStorage.setItem("userConfig", JSON.stringify(userConfig));
-    document.getElementById('user-name').innerHTML = `${data.student_first + " " + data.student_last}`;
-    document.getElementById('user-name').innerHTML = `${data.faculty_first + " " + data.faculty_last}`;
     document.getElementById('user-email').innerHTML = `${data.user_email}`;
-    document.getElementById('user-id').innerHTML = `${data.student_id}`;
-    document.getElementById('user-id').innerHTML = `${data.faculty_id}`;
 
+
+    if(data.user_type === 'student'){
+        document.getElementById('user-name').innerHTML = `${data.student_first + " " + data.student_last}`;
+    } else {
+        document.getElementById('user-name').innerHTML = `${data.faculty_first + " " + data.faculty_last}` ;
+    }
+
+    if(data.user_type === 'student'){
+        document.getElementById('user-id').innerHTML = `${data.student_id}`
+    } else {
+        document.getElementById('user-id').innerHTML = `${data.faculty_id}`
+    }
 }
 
     
