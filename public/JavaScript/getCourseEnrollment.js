38b680dffe95f967322988dae3e31fd98d34a73a
Daniel Rodriguez <daniel.b.rodriguez@gmail.com>
2021-05-24 20:54:42 -0400

    fixed css for planner and enrollment

---
+++ public/JavaScript/getCourseEnrollment.js
@@ -0,0 +1,66 @@
+var userConfig = JSON.parse(localStorage.getItem('userConfig'));
+console.log(userConfig);
+
+getEnrolledCourses();
+async function getEnrolledCourses() {
+    if (userConfig['type'] === 'student') {
+        var response = await fetch(`/students/courses/enrolled/${userConfig['sid']}`);
+	    var data = await response.json();
+        console.log(data);
+
+        //inject here
+        if (data.length) {
+            if (data.length > 1) {
+                document.getElementById('my-enrolled-courses').innerHTML = `
+                ${data.map(function(course) {
+                    return `
+                     <div class="course-entry">
+                    <label for="${course.course_id}">${course.course_name + " Section "+ course.course_section + " "+ course.course_days + " "+ course.course_start_time + "-"+ course.course_end_time}</label>
+                    <button class="status-btn" type="button" name="${course.course_id}" id="${course.course_id}"  value="${course.course_id}">Enrolled</button>
+                    </div>
+                        `
+                }).join('')}
+                   `
+            } else
+                document.getElementById('my-enrolled-courses').innerHTML = `<div class="course-entry">
+                    <label for="${data.course_id}">${data[0].course_name + " Section "+ data[0].course_section + " "+ data[0].course_days + " "+ data[0].course_start_time + "-"+ data[0].course_end_time}</label>                    
+                    <button class="status-btn" type="button" name="${data.course_id}" id="${data.course_id}"  value="${data.course_id}">Enrolled</button>
+                    </div>`;
+        } else {
+            document.getElementById('my-enrolled-courses').innerHTML = `<p>Currently not enrolled to any course</p>`;
+        }
+    }
+}
+
+getPlannedCourses();
+async function getPlannedCourses() {
+    if (userConfig['type'] === 'student') {
+        var response = await fetch(`/students/courses/planned/${userConfig['sid']}`);
+	    var data = await response.json();
+        console.log(data.length);
+
+        //inject here
+        if (data.length) {
+            if (data.length > 1) {
+                document.getElementById('my-planned-courses').innerHTML = `
+                ${data.map(function(course) {
+                    return `
+                     <div class="course-entry">
+                     <button class="status-btn" type="button" id="${course.course_id}" name="${course.course_id}" value="${course.course_id}">Enroll</button>
+                    <p>${course.course_name + " Section "+ course.course_section + " "+ course.course_days + " "+ course.course_start_time + "-"+ course.course_end_time}</p>
+                    <button class="status-btn" type="button" name="class-status-btn" value="Class Status" >Delete</button>
+                   </div>
+                        `
+                }).join('')}
+                   `
+            } else
+                document.getElementById('my-planned-courses').innerHTML = `<div class="course-entry">
+                    <button class="status-btn" type="button" id="${data.course_id}" name="${data.course_id}" value="${data.course_id}" >Add</button>
+                    <p>${data[0].course_name + " Section "+ data[0].course_section + " "+ data[0].course_days + " "+ data[0].course_start_time + "-"+ data[0].course_end_time}</p>                    
+                    <button class="status-btn" type="button" name="class-status-btn" value="Class Status" >Delete</button>
+                    </div>`;
+        } else {
+            document.getElementById('my-planned-courses').innerHTML = `<p>No Planned Courses</p>`;
+        }
+    }
+}
--- public/JavaScript/getCoursePlanner.js
+++ public/JavaScript/getCoursePlanner.js
@@ -46,18 +46,16 @@ async function getPlannedCourses() {
                 ${data.map(function(course) {
                     return `
                      <div class="course-entry">
-                    <button class="status-btn" type="button" name="class-status-btn" value="Class Status" >Delete</button>
                     <label for="${course.course_id}">${course.course_name + " Section "+ course.course_section + " "+ course.course_days + " "+ course.course_start_time + "-"+ course.course_end_time}</label>
-                   <button class="status-btn" type="button" id="${course.course_id}" name="${course.course_id}" value="${course.course_id}">Enroll</button>
+                    <button class="status-btn" type="button" name="class-status-btn" value="Class Status" >Delete</button>
                     </div>
                         `
                 }).join('')}
                    `
             } else
                 document.getElementById('my-planned-courses').innerHTML = `<div class="course-entry">
+                    <label for="${data.course_id}">${data[0].course_name + " Section "+ data[0].course_section + " "+ data[0].course_days + " "+ data[0].course_start_time + "-"+ data[0].course_end_time}</label>
                     <button class="status-btn" type="button" name="class-status-btn" value="Class Status" >Delete</button>
-                    <label for="${data.course_id}">${data[0].course_name + " Section "+ data[0].course_section + " "+ data[0].course_days + " "+ data[0].course_start_time + "-"+ data[0].course_end_time}</label>                    
-                   <button class="status-btn" type="button" id="${data.course_id}" name="${data.course_id}" value="${data.course_id}" >Add</button>
                     </div>`;
         } else {
             document.getElementById('my-planned-courses').innerHTML = `<p>No Planned Courses</p>`;
